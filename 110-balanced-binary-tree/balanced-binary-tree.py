b245da1a32bbe27a40435bcec0406bf1d0b924e6
pranshu <prakashpranshu5@gmail.com>
2026-07-08 04:28:48 +0530

    Time: 3 ms (53.01%) | Memory: 20.5 MB (39.54%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -7,20 +7,22 @@
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root: return True
-        def maxh(node,c):
-            if not node: return c
-            c+=1
-            print(node.val,c)
-            return max(maxh(node.right,c),maxh(node.left,c))
-        #iteration
-        def dfs(root):
-            if not root : return True
-            if abs(maxh(root.right,0)-maxh(root.left,0)) <= 1:
-                return dfs(root.right) and dfs(root.left)
-            else:
-                return False
-        return dfs(root)
-            
 
+        def height(node):
+            if not node:
+                return 0
 
+            left = height(node.left)
+            if left == -1:
+                return -1
+
+            right = height(node.right)
+            if right == -1:
+                return -1
+
+            if abs(left - right) > 1:
+                return -1
+
+            return 1 + max(left, right)
+
+        return height(root) != -1
